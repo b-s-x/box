4e1713392ac52e06767959bd012761eaea622abc
b-s-x <b.s.x.mars@gmail.com>
2020-06-29 09:33:03 +0300

    add event emmiter similar to NodeJS

---
+++ emtr/emitter.js
@@ -0,0 +1,30 @@
+class Emmiter {
+  constructor() {
+    this.events = [];
+  }
+
+  on(type, listener) {
+    this.events[type] = this.events[type] || [];
+    this.events[type].push(listener)
+  }
+
+  emit(type) {
+    if(this.events[type]) {
+      this.events[type].forEach((listener) => {
+        listener();
+      })
+    }
+  }
+};
+
+const emtr = new Emmiter()
+
+emtr.on('greet', () => {
+  console.log('Welcome');
+});
+
+emtr.on('greet', () => {
+  console.log('Greet');
+});
+
+emtr.emit('greet');

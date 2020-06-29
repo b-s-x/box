8abd00e4520319cf1798d7fc48b423d2fa5d7584
b-s-x <b.s.x.mars@gmail.com>
2020-06-29 12:37:20 +0300

    readd design pattern observer

--- pattern/observer/observer.js
+++ pattern/observer/observer.js
@@ -1,41 +1,36 @@
-class Subject {
+ class Subject {
   constructor() {
     this._observers = [];
   }
 
   subscribe(item) {
     this._observers.push(item);
+    console.log('subscribed');
   }
 
   unsubscribe(item) {
     this._observers = this._observers.filter((subscriber) => subscriber !== item );
+    console.log('unsubscribed');
   }
 
   notify(data) {
-    this._observers.forEach((observer) => observer(data));
+    this._observers.forEach((observer) => observer.update(data));
+    console.log('Observers is notified!');
   }
 }
 
 class Observer {
-  update() {
-
+  update(newData) {
+    this._data = newData;
+    console.log('update', newData);
   }
 }
 
+const subject = new Subject();
+const observerA = new Observer();
+const observerB = new Observer();
 
+subject.subscribe(observerA);
+subject.subscribe(observerB);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
+subject.notify({data:'1'});

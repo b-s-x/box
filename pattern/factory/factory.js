c0da520c6a6e03b95297e47698ff6da7fbe37d7f
b-s-x <b.s.x.mars@gmail.com>
2020-06-30 12:59:08 +0300

    add design pattern factory

---
+++ pattern/factory/factory.js
@@ -0,0 +1,35 @@
+const Laptop = function({ ram, hdd, name, ssd }) {
+  this.ram = ram || 0;
+  this.hdd = hdd || 0;
+  this.name = name || 0;
+  this.ssd = ssd || 0;
+}
+
+const Tablet = function({ ram, hdd, name, network }) {
+  this.ram = ram || 0;
+  this.hdd = hdd || 0;
+  this.name = name || 0;
+  this.network = network || 0;
+}
+
+const gadget = { Laptop, Tablet };
+
+function createGadget(type, attr) {
+  const GadgetType = gadget[type];
+  return new GadgetType(attr);
+}
+
+const myLaptopOne = createGadget('Laptop', {
+  ram: 8,
+  ssd: 256,
+  name: 'MacBook',
+});
+
+const myLaptopTwo = createGadget('Laptop', {
+  ram: 8,
+  hdd: 512,
+  name: 'Dell',
+});
+
+console.log(myLaptopOne);
+console.log(myLaptopTwo);

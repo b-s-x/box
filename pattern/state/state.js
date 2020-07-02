e17272eceac24fa7d21bf0d06f23e83eeb7518e7
b-s-x <b.s.x.mars@gmail.com>
2020-07-02 22:12:41 +0300

    add design pattern iterator

---
+++ pattern/state/state.js
@@ -0,0 +1,58 @@
+class OrderStatus {
+  constructor(name, nextStatus) {
+    this._name = name;
+    this._nextStatus = nextStatus;
+  }
+
+  next() {
+    return new this._nextStatus();
+  }
+}
+
+class WaitingForPayment extends OrderStatus {
+  constructor() {
+    super('waitingForPayment', Shipping)
+  }
+};
+
+class Shipping extends OrderStatus {
+  constructor() {
+    super('shipping', Delivered)
+  }
+};
+
+class Delivered extends OrderStatus {
+  constructor() {
+    super('delivered', Delivered)
+  }
+}
+
+class Order {
+  constructor() {
+    this._state = new WaitingForPayment();
+  }
+
+  nextState() {
+    this._state = this._state.next();
+  }
+
+  cancelOrder() {
+    this._state._name === 'waitingForPayment'
+    ? console.log('Order is canceled')
+    : console.log("Order can't be canceled");
+  }
+}
+
+const myOrder = new Order();
+
+console.log(myOrder._state._name);
+
+myOrder.cancelOrder()
+
+myOrder.nextState();
+console.log(myOrder._state._name);
+
+myOrder.cancelOrder()
+
+myOrder.nextState();
+console.log(myOrder._state._name);

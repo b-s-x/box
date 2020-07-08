671a6675e33ea5fd82f695d08312ba948e1cb3f8
b-s-x <b.s.x.mars@gmail.com>
2020-07-08 16:14:05 +0300

    fix merge_sort

--- sort/merge_sort.js
+++ sort/merge_sort.js
@@ -1,37 +1,31 @@
 const merge = (arrFirst, arrSecond) => {
     const arrSort = [];
-    let i = j = 0;
-    // сравниваем два массива, поочередно сдвигая указатели
-    while (i < arrFirst.length && j < arrSecond.length) {
-        arrSort.push(
-            (arrFirst[i] < arrSecond[j]) ?
-                arrFirst[i++] : arrSecond[j++]
-        );
+    let iFirst = iSecond = 0;
+
+    while (iFirst < arrFirst.length && iSecond < arrSecond.length) {
+        if(arrFirst[iFirst] < arrSecond[iSecond]) {
+          arrSort.push(arrFirst[iFirst++])
+        } else {
+          arrSort.push(arrSecond[iSecond++])
+        }
     }
-    // обрабатываем последний элемент при разной длине массивов
-    // и возвращаем один отсортированный массив
     return [
         ...arrSort,
-        ...arrFirst.slice(i),
-        ...arrSecond.slice(j)
+        ...arrFirst.slice(iFirst),
+        ...arrSecond.slice(iSecond)
     ];
 };
 
 const mergeSort = arr => {
-    // Проверяем корректность переданных данных
     if (!arr || !arr.length) {
         return null;
     }
-    //Если массив содержит один элемент просто возвращаем его
     if (arr.length <= 1) {
         return arr;
     }
-    // Находим середину массива и делим его на два
     const middle = Math.floor(arr.length / 2);
     const arrLeft = arr.slice(0, middle);
     const arrRight = arr.slice(middle);
-    // Для новых массивов снова вызываем сортировку,
-    // сливаем их и возвращаем снова единый массив
     return merge(mergeSort(arrLeft), mergeSort(arrRight));;
 };
 

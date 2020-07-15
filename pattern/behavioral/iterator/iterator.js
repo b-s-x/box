06ef56cf1af694cb7d799814bdc1ade1308b4153
b-s-x <b.s.x.mars@gmail.com>
2020-07-15 18:45:45 +0300

    rewrite function

--- pattern/behavioral/iterator/iterator.js
+++ pattern/behavioral/iterator/iterator.js
@@ -1,50 +1,64 @@
 class IteratorOne {
   constructor(elements) {
-    this._index = 0;
-    this._elements = elements
+      this._index = 0;
+      this._elements = elements
   }
 
   next() {
-    return this._elements[this._index++]
+      return this._elements[this._index++]
   }
 
   hasNext() {
-    return this._index < this._elements.length;
+      return this._index < this._elements.length;
   }
 }
 
-const collectionOne = new IteratorOne(['1', '2', '3', '4', '5'])
+const runOne = () => {
 
-while(collectionOne.hasNext()) {
-  console.log(collectionOne.next());
+  const elements = [1, '2', 3, '4', 5]
+
+  const collection = new IteratorOne(elements)
+
+  while(collection.hasNext()) {
+      console.log(collection.next());
+  }
 }
+
+runOne();
 
 // __________________________________________________________
 
 class IteratorTwo {
-  constructor(elements) {
-    this._index = 0;
-    this._keys = Object.keys(elements);
-    this._elements = elements;
+  constructor(arr) {
+      this._index = 0;
+      this._keys = Object.keys(arr);
+      this._arr = arr;
   }
 
   next() {
-    return this._elements[this._keys[this._index++]];
+      return this._arr[this._keys[this._index++]];
   }
 
   hasNext() {
-    return this._index < this._keys.length
+      return this._index < this._keys.length;
   }
 };
 
-const autos = {
-  audi: { model: 'Audi', color: 'black', price: '20000'},
-  bmw: { model: 'BMW', color: 'white', price: '30000'},
-  tesla: { model: 'Tesla', color: 'black', price: '40000'},
+const order = {
+  bigMac: { type: 'burger', size: '2', price: '139'},
+  makNaggets: { type: 'naggets', quantity: '6', price: '90'},
+  fries: { type: 'fries', size: 'standard', price: '69'},    
+  makCoffee: { type: 'coffee', size: 'standard', price: '89', sugar: false},    
 };
 
-const collectionTwo = new IteratorTwo(autos);
 
-while(collectionTwo.hasNext()) {
-  console.log(collectionTwo.next());
+const runTwo = () => {
+  
+  const collectionTwo = new IteratorTwo(order);
+
+  while(collectionTwo.hasNext()) {
+      console.log(collectionTwo.next());
+  };
 };
+
+runTwo();

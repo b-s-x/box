49034c8b09c836ad97b633a199457b3846f9b2c3
b-s-x <b.s.x.mars@gmail.com>
2020-07-16 20:00:20 +0300

    add data structure binary heap

---
+++ data_structure/binary_heap/binary_heap.js
@@ -0,0 +1,31 @@
+class BinaryHeap {
+    constructor() {
+      this.values = [];
+    }
+  
+    add(element) {
+      this.values.push(element) 
+      let index = this.values.length - 1;
+      const current = this.values[index];
+  
+      while(index > 0) {
+        let parentIndex = Math.floor((index - 1) / 2);
+        let parent = this.values[parentIndex];
+  
+        if(parent <= current) {
+          this.values[parentIndex] = current;
+          this.values[index] = parent;
+          index = parentIndex;
+        } else break
+      }
+    }
+  }
+  
+  const tree = new BinaryHeap();
+  tree.add(3)
+  tree.add(4)
+  tree.add(31)
+  tree.add(6)
+  tree.add(1)
+  console.log(tree);
+  

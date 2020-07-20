93f711ba420db0d6fd7de275733e4ca674f53455
b-s-x <b.s.x.mars@gmail.com>
2020-07-21 01:33:05 +0300

    edit stack function pop

--- data_structure/stack/stack.js
+++ data_structure/stack/stack.js
@@ -1,32 +1,31 @@
 class Stack {
-    constructor() {
-      this.count = 0;
-      this.storage = {};
-    }
-  
-    push(value) {
-      this.storage[this.count] = value;
-      this.count++
-    }
-  
-    pop() {
-      if(this.count === 0) return;
-      this.count--
-      let result = this.storage[this.count];
-      delete this.storage[this.count];
-      return result;
-    }
-    
-    peek() {
-      console.log(this.storage);
-      return this.storage[this.count - 1];
-    }
-  
-    size() {
-      console.log(this.count);
-      return this.count;
-    }
+  constructor() {
+    this.count = 0;
+    this.storage = {};
   }
+
+  push(value) {
+    this.storage[this.count] = value;
+    this.count++
+  }
+
+  pop() {
+    if(this.count === 0) return;
+    this.count--
+    delete this.storage[this.count];
+    return this.storage
+  }
+  
+  peek() {
+    console.log(this.storage);
+    return this.storage[this.count - 1];
+  }
+
+  size() {
+    console.log(this.count);
+    return this.count;
+  }
+}
   
   const stack = new Stack();
   

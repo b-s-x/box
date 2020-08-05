9fcad2ae41131155edc4327b131ccc63843c4c7d
b-s-x <b.s.x.mars@gmail.com>
2020-08-05 14:02:12 +0300

    rewrite function constructor on class

--- pattern/creational/factory/factory_example_one.js
+++ pattern/creational/factory/factory_example_one.js
@@ -1,34 +1,38 @@
-const Laptop = function({ ram, hdd, name, ssd }) {
-  this.ram = ram || 0;
-  this.hdd = hdd || 0;
-  this.name = name || 0;
-  this.ssd = ssd || 0;
+class Laptop {
+  constructor({ ram, hdd, name, ssd }) {
+      this.ram = ram || 0;
+      this.hdd = hdd || 0;
+      this.name = name || 0;
+      this.ssd = ssd || 0;
+  }
 }
 
-const Tablet = function({ ram, hdd, name, network }) {
-  this.ram = ram || 0;
-  this.hdd = hdd || 0;
-  this.name = name || 0;
-  this.network = network || 0;
+class Tablet {
+  constructor({ram, hdd, name, network}) {
+      this.ram = ram || 0;
+      this.hdd = hdd || 0;
+      this.name = name || 0;
+      this.network = network || 0;
+  }
 }
 
 const gadget = { Laptop, Tablet };
 
 function createGadget(type, attr) {
-  const GadgetType = gadget[type];
-  return new GadgetType(attr);
+const GadgetType = gadget[type];
+return new GadgetType(attr);
 }
 
 const myLaptopOne = createGadget('Laptop', {
-  ram: 8,
-  ssd: 256,
-  name: 'MacBook',
+ram: 8,
+ssd: 256,
+name: 'MacBook',
 });
 
 const myLaptopTwo = createGadget('Laptop', {
-  ram: 8,
-  hdd: 512,
-  name: 'Dell',
+ram: 8,
+hdd: 512,
+name: 'Dell',
 });
 
 console.log(myLaptopOne);

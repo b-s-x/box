9ce8092e55d9c64dca10a9c508af1a5126b04300
b-s-x <b.s.x.mars@gmail.com>
2020-08-05 14:51:00 +0300

    simplified example, rewrite on classes

--- pattern/creational/factory/simple_factory.js
+++ pattern/creational/factory/simple_factory.js
@@ -1,23 +1,27 @@
 class Cat {
-  constructor(name, color) {
-    this._name = name;
-    this._color = color;
-  };
-
-  getName() {
-    return this._name;
-  };
-
-  getColor() {
-    return this._color;
-  };
+    constructor(name, color) {
+        this._name = name;
+        this._color = color;
+    };
+  
+    getName() {
+        return this._name;
+    };
+  
+    getColor() {
+        return this._color;
+    };
+};
+  
+class CatFactory {
+    make(name, color) {
+        return new Cat(name, color)
+    }
 };
 
-const CatFactory = {
-  makeCat: (name, color) => new Cat(name, color)
-};
-
-const barsik = CatFactory.makeCat('Barsik', 'black');
+const cat = new CatFactory();
+const barsik = cat.make('Barsik', 'black');
 
 console.log(barsik);
 console.log(barsik._name);
+  

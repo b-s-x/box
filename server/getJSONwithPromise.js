b39639094be2254643a961fedbffced8b0adb19b
b-s-x <b.s.x.mars@gmail.com>
2020-09-27 19:57:53 +0300

    get JSON, saved, readed and parse JSON with promise

---
+++ server/getJSONwithPromise.js
@@ -0,0 +1,35 @@
+const requestPromise = require('request-promise')
+const fs = require('fs');
+
+const URL = "https://jsonplaceholder.typicode.com/comments"
+
+const readStream = () => {
+    let data = '';
+
+    let reader = fs.createReadStream('data.txt', 'UTF8')
+    .on('data', (chunk) => {
+        data += chunk
+    })
+    .on('end', () => {
+        let json = JSON.parse(data);
+    })
+}
+
+const stream = async (data) => {
+    let writeStream = fs.createWriteStream("data.txt");
+    writeStream.write(data)
+    await readStream()
+}
+
+const options = {
+    method: 'GET',
+    url: URL,
+}
+
+requestPromise(options)
+    .then((response) => {
+        stream(response)
+    })
+    .catch((err) => {
+        console.log(err);
+    })

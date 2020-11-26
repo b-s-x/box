b2c985d95e3199685bf8c6d8bde112ce380277bd
b-s-x <b.s.x.mars@gmail.com>
2020-11-26 16:22:14 +0300

    xz

--- pattern/creational/factory/factory_example_two.js
+++ pattern/creational/factory/factory_example_two.js
@@ -1,16 +1,16 @@
 class Server {
-    getDescriprion() {
+    getDescriprtion() {
         console.log('server run');
     }
 };
-  
+
 class BackendPerson {
     constructor(name, age) {
         this._name = name;
         this._age = age;
     }
 
-    getDescriprion() {
+    getDescriprtion() {
         console.log(`Developer name: ${this._name}, age: ${this._age}`);
         return this._name, this._age;
     }
@@ -20,16 +20,16 @@ class BackendFactory {
     makeServer() {
         return new Server();
     };
-  
+
     backendPerson(name, age) {
         return new BackendPerson(name, age);
     }
 };
-  
+
 const backendFactory = new BackendFactory();
-  
+
 const server = backendFactory.makeServer();
 const expert = backendFactory.backendPerson('bsx', 25);
 
-server.getDescriprion();
-expert.getDescriprion();
+server.getDescriprtion();
+expert.getDescriprtion();

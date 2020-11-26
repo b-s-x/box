588c0a42cb3abe16fe7379ab209308ac35686815
b-s-x <b.s.x.mars@gmail.com>
2020-11-26 16:30:31 +0300

    create function shuffle

---
+++ task/shuffle.js
@@ -0,0 +1,21 @@
+// Fisher–Yates shuffle
+function shuffle(arr) {
+  const newArr = arr.map((item) => item);
+
+  for(let item = newArr.length - 1; item > 0; item--) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    [newArr[item], newArr[randomIndex]] = [newArr[randomIndex], newArr[item]];
+  }
+
+  return newArr;
+}
+
+const run = () => {
+  const arr = [1, 2, 3, 4, 5, 6, 7];
+  const x = shuffle(arr);
+  console.log(arr);
+  console.log(x);
+};
+
+run();
+

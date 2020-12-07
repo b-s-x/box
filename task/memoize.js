41d43ebcc595374d7d96870c6af121b332e8023d
b-s-x <b.s.x.mars@gmail.com>
2020-12-07 13:00:54 +0300

    create memoize

---
+++ task/memoize.js
@@ -0,0 +1,25 @@
+const memoize = (func) => {
+  const cache = {};
+
+  return (n) => {
+    if(n in cache) return cache[n]
+
+    return cache[n] = func(n)
+  }
+}
+
+const factorial = memoize((n) => {
+  let prev = 0;
+  let next = 1;
+  for(let i = 0; i < n; i++){
+    console.log(i);
+      let temp = next;
+      next = prev + next;
+      prev = temp;
+    }
+
+    return prev;
+  })
+
+console.log(factorial(5));
+console.log(factorial(5));

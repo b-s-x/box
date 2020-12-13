61241c7b0693c8fe0b7058afa8bd76b253466724
b-s-x <b.s.x.mars@gmail.com>
2020-12-13 18:53:16 +0300

    added new example singleton pattern

---
+++ pattern/creational/singleton/singleton_safe_two.js
@@ -0,0 +1,17 @@
+const Singleton = (() => {
+  let instance;
+
+  class Singleton {
+    constructor() {
+      if (instance) return instance;
+      instance = this;
+    }
+  }
+
+  return Singleton;
+})();
+
+// Usage
+
+console.assert(new Singleton() === new Singleton())
+console.log('instances are equal')
